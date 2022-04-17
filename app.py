53a45fb9f27b75801e522b93fde0d97e13b31c6e
erikagonzalez5 <erikagonzalez5@gmail.com>
2022-04-16 18:52:44 -0700

    adding again

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 
-
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
-# from joblib import load
+from joblib import load
 import pandas as pd
-
-# import matplotlib.pyplot as plt
 # import plotly.express as px
 # import plotly.graph_objects as go
 import uuid
-# import tensorflow as tf
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers import LSTM
-# from keras.layers import Dropout
+from input_v2 import Model
+import yfinance as yf
 
 app = Flask(__name__)
 
@@ -41,27 +35,28 @@ def calendar():
 @app.route('/', methods=['GET', 'POST'])
 def main():
     
-    
     # If a form is submitted
     if request.method == "POST":
         
         # Unpickle classifier
-        model = joblib.load(model, "model.pkl")
+        model = joblib.load("model.pkl")
+        
+
         
         # Get values through input bars
         ticker = request.form.get("Ticker")
-       
-        
-        # Put inputs to dataframe
-        X = pd.DataFrame([[ticker]], columns = ["Ticker"])
-        
+        SPF = Model()
+        data = Model.extract_data(ticker)
+        X = Model.reshape()
+
+
         # Get prediction
-        prediction = model.predict(X)[0]
+        prediction = model.predict(X)
         
     else:
         prediction = ""
         
-    return render_template("index.html")
+    return render_template("index.html", output = prediction)
 
 
 # Running the app
