b1a2182bc48881aed2ed9ebcb46491e85a993c7c
Alexandra  Baker <bakera@alumni.usc.edu>
2022-04-18 19:34:56 -0700

    cleaned up repo, trying to merge

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request
-import numpy as np
-from joblib import load
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import uuid
-
-app = Flask(__name__)
-
-=======
 
 
 from flask import Flask, render_template, request
@@ -49,24 +37,16 @@ def crypto():
 def calendar():
     app.static_folder = 'static'
     return render_template("calendar.html")
->>>>>>> 64baa503ec738c0f2584164369c0fe07ec96f860
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     
-<<<<<<< HEAD
-=======
     
->>>>>>> 64baa503ec738c0f2584164369c0fe07ec96f860
     # If a form is submitted
     if request.method == "POST":
         
         # Unpickle classifier
-<<<<<<< HEAD
-        model = joblib.load("model.pkl")
-=======
         model = joblib.load(model, "model.pkl")
->>>>>>> 64baa503ec738c0f2584164369c0fe07ec96f860
         
         # Get values through input bars
         ticker = request.form.get("Ticker")
@@ -81,11 +61,7 @@ def main():
     else:
         prediction = ""
         
-<<<<<<< HEAD
-    return render_template("index.html", output = prediction)
-=======
     return render_template("index.html")
->>>>>>> 64baa503ec738c0f2584164369c0fe07ec96f860
 
 
 # Running the app
