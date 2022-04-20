9ec19a0357b06012ed1717b53ba2c831ae6e4de7
erikagonzalez5 <erikagonzalez5@gmail.com>
2022-04-19 19:56:08 -0700

    working flask

--- app_v3.py
+++ app_v3.py
@@ -1,4 +1,91 @@
 
+# from flask import Flask, render_template, request
+# import numpy as np
+# import joblib
+# from joblib import load
+# import pandas as pd
+# # import plotly.express as px
+# # import plotly.graph_objects as go
+# # import uuid
+# from input_v2 import Model
+# import yfinance as yf
+# from keras.models import load_model
+# from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# #load model
+# model = load_model("model.h5")
+
+# app = Flask(__name__)
+
+# # @app.route('/index')
+# # def index():
+# #     app.static_folder = 'static'
+# #     return render_template("index.html")
+
+# # @app.route('/getstarted')
+# # def getstarted():
+# #     app.static_folder = 'static'
+# #     return render_template("getstarted.html")
+
+# # @app.route('/crypto')
+# # def crypto():
+# #     app.static_folder = 'static'
+# #     return render_template("crypto.html")
+
+# # @app.route('/calendar')
+# # def calendar():
+# #     app.static_folder = 'static'
+# #     return render_template("calendar.html")
+
+# @app.route('/', methods=['GET', 'POST'])
+# def main():
+    
+#  # If a form is submitted
+#     if request.method == "POST":
+        
+        
+#         # Get values through input bars
+#         ticker = request.form.get("Ticker")
+#         SPF = Model()
+#         data = SPF.extract_data(ticker)
+#         X = SPF.reshape()
+
+#         scaler = MinMaxScaler(feature_range = (0, 1))
+#         # Get prediction
+#         prediction = model.predict(X)
+#         prediction = scaler.inverse_transform(prediction)
+        
+#     else:
+#         prediction = ""
+        
+#     return render_template("index.html", output = prediction)
+
+# @app.route('/getstarted')
+# def getstarted():
+#     app.static_folder = 'static'
+#     return render_template("getstarted.html")
+
+# @app.route('/crypto')
+# def crypto():
+#     app.static_folder = 'static'
+#     return render_template("crypto.html")
+
+# @app.route('/calendar')
+# def calendar():
+#     app.static_folder = 'static'
+#     return render_template("calendar.html")
+
+# @app.route('/sp500')
+# def sp500():
+#     app.static_folder = 'static'
+#     return render_template("s&p500.html")
+
+
+# # Running the app
+# if __name__ == '__main__':
+#     app.run(debug = True)
+
+
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
@@ -7,7 +94,7 @@ import pandas as pd
 # import plotly.express as px
 # import plotly.graph_objects as go
 # import uuid
-from input_v2 import Model
+from input_v3 import Model
 import yfinance as yf
 from keras.models import load_model
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
@@ -48,17 +135,16 @@ def main():
         ticker = request.form.get("Ticker")
         SPF = Model()
         data = SPF.extract_data(ticker)
-        X = SPF.reshape()
-
-        scaler = MinMaxScaler(feature_range = (0, 1))
         # Get prediction
-        prediction = model.predict(X)
-        prediction = scaler.inverse_transform(prediction)
+        stock_prediction = SPF.reshape_model()
+
+        # Get prediction
+        
         
     else:
         prediction = ""
         
-    return render_template("index.html", output = prediction)
+    return render_template("index.html", output = stock_prediction)
 
 @app.route('/getstarted')
 def getstarted():
--- input_v2.py
+++ input_v2.py
@@ -1,3 +1,96 @@
+# # !pip install yahoo_fin
+# # !pip install yfinance
+# # !pip install yahoofinancials
+
+
+# import numpy as np
+# import requests
+# import datetime
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# # import yahoo_fin.stock_info as si
+# import yfinance as yf
+# # from yahoofinancials import YahooFinancials
+
+# class Model():
+#     '''
+#     A model for predicting the stock price
+#     '''
+#     def __init__(self):
+#         '''
+#         starting out 
+#         '''
+        
+        
+#     def extract_data(self, ticker):
+#         dl_df= yf.download(ticker, start='2021-01-01', 
+#                     end='2022-04-01', progress=False)
+    
+#         self.ticker_set = pd.DataFrame(dl_df)
+#         self.ticker_set.reset_index(inplace = True)
+#         return self.ticker_set
+
+
+#     def reshape(self):
+
+#         stock_top_df = pd.read_csv("dataframes_top.csv")
+#         dataset_total = pd.concat((stock_top_df['Open'], self.ticker_set['Open']), axis = 0)
+#         # Extract Stock Prices for Test time period, plus 60 days previous
+#         inputs = dataset_total[len(dataset_total) - len(self.ticker_set) - 60:].values
+#         # 'reshape' function to get it into a NumPy format
+#         inputs = inputs.reshape(-1,1)
+#         # Scaling the input
+#         scaler = MinMaxScaler(feature_range = (0, 1))
+#         inputs = scaler.fit_transform(inputs)
+
+#         X_test = []
+
+#         for i in range(60, len(inputs)):
+#             X_test.append(inputs[i-60:i, 0])
+
+#         X_test = np.array(X_test)
+#         # Making the input in 3D format
+#         X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#                 return X_test
+
+
+# # Graph Function
+# # def make_graph(self, ticker, model, output_file='predictions_pic.svg'):
+# #   date = self.ticker_set['Date']
+# #   Last314Days = date.tail(314)
+# #   real_df = self.ticker_set['Open']
+# #   real_df.columns = ['Real']
+# #   predicted_df = prediction
+# #   predicted_df.columns = ['Predicted']
+# #   line_df = pd.concat([Last314Days, real_df, predicted_df], axis=1)
+# #   fig = go.Figure([
+# #                  go.Scatter(
+# #                      x=line_df['Date'],
+# #                      y=line_df[f'Real {ticker}'],
+# #                     showlegend=True,
+# #                     name=f'Real {ticker}'
+# #                  ),
+# #                  go.Scatter(
+# #                      x=line_df['Date'],
+# #                      y=line_df[f'Predicted {ticker}'],
+# #                     showlegend=True,
+# #                     name=f'Predicted {ticker}'
+# #                  )
+# #   ])
+
+# #   fig.update_layout(
+# #                     yaxis_title = 'Stock Prices (USD)',
+# #       xaxis_title='Dates',
+# #       title=f'Real and Predicted {ticker} Stock Prices',
+# #       hovermode="x"
+# #   )
+# #   fig.write_image(output_file, width=800)
+# #   return fig
+
+
 # !pip install yahoo_fin
 # !pip install yfinance
 # !pip install yahoofinancials
@@ -14,6 +107,9 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 # import yahoo_fin.stock_info as si
 import yfinance as yf
 # from yahoofinancials import YahooFinancials
+
+#load model
+model = load_model("model.h5")
 
 class Model():
     '''
@@ -34,7 +130,7 @@ class Model():
         return self.ticker_set
 
 
-    def reshape(self):
+    def reshape_model(self):
 
         stock_top_df = pd.read_csv("dataframes_top.csv")
         dataset_total = pd.concat((stock_top_df['Open'], self.ticker_set['Open']), axis = 0)
@@ -54,41 +150,16 @@ class Model():
         X_test = np.array(X_test)
         # Making the input in 3D format
         X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-                return X_test
+
+        self.prediction = model.predict(X_test)
+        self.results = scaler.inverse_transform(self.prediction)
+
+        return self.results
+
+        
 
 
-# Graph Function
-# def make_graph(self, ticker, model, output_file='predictions_pic.svg'):
-#   date = self.ticker_set['Date']
-#   Last314Days = date.tail(314)
-#   real_df = self.ticker_set['Open']
-#   real_df.columns = ['Real']
-#   predicted_df = prediction
-#   predicted_df.columns = ['Predicted']
-#   line_df = pd.concat([Last314Days, real_df, predicted_df], axis=1)
-#   fig = go.Figure([
-#                  go.Scatter(
-#                      x=line_df['Date'],
-#                      y=line_df[f'Real {ticker}'],
-#                     showlegend=True,
-#                     name=f'Real {ticker}'
-#                  ),
-#                  go.Scatter(
-#                      x=line_df['Date'],
-#                      y=line_df[f'Predicted {ticker}'],
-#                     showlegend=True,
-#                     name=f'Predicted {ticker}'
-#                  )
-#   ])
-
-#   fig.update_layout(
-#                     yaxis_title = 'Stock Prices (USD)',
-#       xaxis_title='Dates',
-#       title=f'Real and Predicted {ticker} Stock Prices',
-#       hovermode="x"
-#   )
-#   fig.write_image(output_file, width=800)
-#   return fig
 
 
                 
+
